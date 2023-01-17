8a29d90c2fec04715159bff9001f3d781a10d632
Jaredglass3 <Jaredglass352@gmail.com>
2023-01-17 17:02:09 +0000

    Task 1 of the project

---
+++ 0x04-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This is the Square module.
+The Square module contains the Square class
+"""
+
+
+class Square:
+    """
+    This is the Square class
+    """
+    def __init__(self, size):
+        """
+        The __init__ method
+        """
+        self.__size = size
