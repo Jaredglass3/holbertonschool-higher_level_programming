7d7fc8efa25630d6093816d1f5a3de00a94b442e
Jaredglass3 <Jaredglass352@gmail.com>
2023-01-30 05:42:28 +0000

    Made corrections

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
-# 3-is_kind_of_class.py
-# Brennan D Baraban <375@holbertonschool.com>
-"""Defines a class and inherited class-checking function."""
+# 0-lookup.py
+"""Defines an object attribute lookup function."""
 
 
-def is_kind_of_class(obj, a_class):
-    """Check if an object is an instance or inherited instance of a class.
-    Args:
-        obj (any): The object to check.
-        a_class (type): The class to match the type of obj to.
-    Returns:
-        If obj is an instance or inherited instance of a_class - True.
-        Otherwise - False.
-    """
-    if isinstance(obj, a_class):
-        return True
-    return False
+def lookup(obj):
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
