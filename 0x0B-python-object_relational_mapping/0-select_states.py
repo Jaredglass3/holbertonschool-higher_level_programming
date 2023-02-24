3fc37c85b4ee831d7cff55e24384400410ddc1f8
Jaredglass3 <Jaredglass352@gmail.com>
2023-02-24 16:39:37 -0600

    Attempted corrections

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -4,33 +4,33 @@ Task 0: lists all states from the database hbtn_0e_0_usa
 """
 
 if __name__ == "__main__":
-        import sys
-            import MySQLdb
+    import sys
+    import MySQLdb
 
-                mysql_username = sys.argv[1]
-                    mysql_pwd = sys.argv[2]
-                        mysql_dbname = sys.argv[3]
+    mysql_username = sys.argv[1]
+    mysql_pwd = sys.argv[2]
+    mysql_dbname = sys.argv[3]
 
-                            # to connect my target db: establish the connector:
-                                my_db = MySQLdb.connect(
-                                            host="localhost",
-                                            port=3306,
-                                            user=mysql_username,
-                                            password=mysql_pwd,
-                                            # db I want to connect to:
-                                            db=mysql_dbname)
+    # to connect my target db: establish the connector:
+    my_db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        password=mysql_pwd,
+        # db I want to connect to:
+        db=mysql_dbname)
 
-                                    # to execute the query of interest --> my SQL request:
-                                        qry_cursor = my_db.cursor()
+    # to execute the query of interest --> my SQL request:
+    qry_cursor = my_db.cursor()
 
-                                            sql_request = "SELECT * FROM states ORDER BY id ASC"
-                                                qry_cursor.execute(sql_request)
+    sql_request = "SELECT * FROM states ORDER BY id ASC"
+    qry_cursor.execute(sql_request)
 
-                                                    # fetchall statement selects all data from the state table:
-                                                        records = qry_cursor.fetchall()
+    # fetchall statement selects all data from the state table:
+    records = qry_cursor.fetchall()
 
-                                                            # print all records:
-                                                                for element in records:
-                                                                            print(element)
+    # print all records:
+    for element in records:
+        print(element)
 
-                                                                            
+    my_db.close()
